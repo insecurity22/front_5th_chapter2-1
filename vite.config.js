f30cabdc1fe99feb58a528554b0b6ad77e8d582e
insecurity22 <insecurity@naver.com>
2025-04-18 04:39:49 +0900

    chore: deploy setting

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  base: '/front_5th_chapter2-1/',
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: 'src/setupTests.js'
+    setupFiles: 'src/setupTests.js',
   },
-})
+});
